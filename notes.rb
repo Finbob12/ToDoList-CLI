5b4e0c75f09c9a6eeb35676272e93e5e0f035ee4
Fin <finlaytgeorge@gmail.com>
2020-09-29 08:38:46 +1000

    fixed TTY prompt issue, needed a downcase

--- notes.rb
+++ notes.rb
@@ -3,7 +3,7 @@ require "tty-prompt"
 
 begin
     prompt = TTY::Prompt.new
-    input = prompt.select("Please select on option", %w(New Update List Show Delete))
-    command, params = input
+    input = prompt.select("Please select on option", %w(New Update List Show Delete Quit))
+    command, params = input.downcase
     dispatch command, params
 end until ['quit', 'q', 'exit', ':q'].include? command
